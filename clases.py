086230a164d7c0d83ab2af12eb6e24c197b2738a
Elias Vasquez <evasq710@hotmail.com>
2021-10-24 22:39:18 -0600

    Clases Arbol n-ario de derivación - Clases Nodos Graphviz

--- clases.py
+++ clases.py
@@ -78,37 +78,69 @@ class Error:
             self.fila = "-"
         if self.columna is None:
             self.columna = "-"
+
+class Arbol_Graphviz:
+    def __init__(self, str_nodos):
+        self.str_graphviz = '''digraph G {
+        graph[label="Arbol de derivacion"]
+        node[style="filled", fillcolor="palegreen"]\n'''
+        self.str_graphviz += str_nodos
+        self.str_graphviz += '}'
+
+class Dato:
+    def __init__(self, id_node, label, inicio = False, no_terminal = False, id_nodo_padre = None):
+        self.id_node = id_node
+        self.label = label
+        self.inicio = inicio
+        self.no_terminal = no_terminal
+        self.id_nodo_padre = id_nodo_padre
+
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.nodos_hijo = []
+        self.str_nodos_graphviz = ''
     
-class Pila:
-    def __init__(self):
-        self.pila = []
+    def insertar_hijo(self, dato):
+        nodo_hijo = Nodo(dato)
+        self.nodos_hijo.append(nodo_hijo)
+
+    def obtener_nodo(self, id_node):
+        if self.dato.id_node == id_node:
+            return self
+        for nodo_hijo in self.nodos_hijo:
+            nodo = nodo_hijo.obtener_nodo(id_node)
+            if nodo is not None:
+                return nodo
+        return None
     
-    def guardar(self, valor = None, lista_valores = None):
-        if valor:
-            self.pila.append(valor)
-        elif lista_valores:
-            self.pila.extend(lista_valores)
-    
-    def sacar(self, valor):
-        if self.pila:
-            if self.pila[-1] == valor:
-                self.pila.pop()
-                return True
+    def insertar_hijo_en(self, dato, id_node_padre):
+        nodo_padre = self.obtener_nodo(id_node_padre)
+        if nodo_padre is not None:
+            nodo_padre.insertar_hijo(dato)
+            return True
+        print(f"No se encontró al nodo interior con id {id_node_padre}")
         return False
     
-    def reemplazar(self, antiguo, nuevo = None, lista_nuevos = None):
-        for index, valor in enumerate(self.pila):
-            if valor == antiguo:
-                if nuevo:
-                    self.pila[index] = nuevo
-                elif lista_nuevos:
-                    self.pila.pop(index)
-                    lista_nuevos.reverse()
-                    i = index
-                    for new in lista_nuevos:
-                        self.pila.insert(i, new)
-                        i += 1
+    def imprimir_arbol(self):
+        print(f"Mi id: {self.dato.id_node}, mi label: {self.dato.label}, mi padre: {self.dato.id_nodo_padre}")
+        if self.nodos_hijo:
+            print(">> Soy No Terminal, y mis hijos son:")
+            for nodo_hijo in self.nodos_hijo:
+                nodo_hijo.imprimir_arbol()
+        else:
+            print(">> Soy Terminal, por lo que no tengo hijos")
     
-    def recorrer(self):
-        for element in self.pila:
-            print(element)
+    def crear_nodos_graphviz(self):
+        if self.dato.inicio:
+            self.str_nodos_graphviz += f'node{self.dato.id_node}[label="{self.dato.label}", fillcolor="cyan3"];\n'
+        elif self.dato.no_terminal:
+            self.str_nodos_graphviz += f'node{self.dato.id_node}[label="{self.dato.label}", fillcolor="coral2"];\n'
+            self.str_nodos_graphviz += f'node{self.dato.id_nodo_padre} -> node{self.dato.id_node};\n'
+        else:
+            self.str_nodos_graphviz += f'node{self.dato.id_node}[label="{self.dato.label}"];\n'
+            self.str_nodos_graphviz += f'node{self.dato.id_nodo_padre} -> node{self.dato.id_node};\n'
+        if self.nodos_hijo:
+            for nodo_hijo in self.nodos_hijo:
+                self.str_nodos_graphviz += nodo_hijo.crear_nodos_graphviz()
+        return self.str_nodos_graphviz
